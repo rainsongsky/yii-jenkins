a0b4840b7fe26b4efbdf223da4cdf5d8dee2492d
Vladimir Drizheruk <vdrizheruk@magecore.com>
2014-07-08 19:55:14 +0300

    build.xml

--- protected/tests/unit/DemoTest.php
+++ protected/tests/unit/DemoTest.php
@@ -12,7 +12,7 @@ class DemoTest extends CTestCase
     public function testMailingToOnePerson()
     {
         $count = 1;
-        $this->assertEquals(1, $count);
+        $this->assertEquals(25, $count);
     }
 
     public function testA()
