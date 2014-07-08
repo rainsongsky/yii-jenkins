da715d16a8ba7a9958475d6b08ccaba979ca7b08
Vladimir Drizheruk <vladimir@drizheruk.com.ua>
2014-07-08 22:01:22 +0300

    test

--- protected/tests/unit/DemoTest.php
+++ protected/tests/unit/DemoTest.php
@@ -1,23 +1,15 @@
 <?php
 
-/**
- * Created by JetBrains PhpStorm.
- * User: drizheruk
- * Date: 7/15/13
- * Time: 7:52 PM
- * To change this template use File | Settings | File Templates.
- */
 class DemoTest extends CTestCase
 {
     public function testMailingToOnePerson()
     {
         $count = 1;
-        $this->assertEquals(25, $count);
+        $this->assertEquals(1, $count);
     }
 
     public function testA()
     {
-        $this->assertTrue(true, false);
+        $this->assertTrue(true, true);
     }
-
 }
